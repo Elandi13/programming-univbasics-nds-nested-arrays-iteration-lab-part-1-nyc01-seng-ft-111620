deab61777f36d1e3bc2700410439aea9696fdf4b
Elvis Landi <elandi13@yahoo.com>
2020-11-07 16:27:55 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,6 +5,9 @@ def find_even_values(src)
 row_index = 0 
 while row index < src.length do 
   element_index = 0
-  while element_index < 
-
+  while element_index < src[row_index].count do
+    element_index += 1
+  end
+  row_index += 1
+ end
 end
